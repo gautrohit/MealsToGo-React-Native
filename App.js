e50e6df70ee1c3009ab963d7fcf917d54d63a476
Rohit <gautamrohit55@gmail.com>
2022-11-20 12:44:14 +0530

    added SafeAreaView, StatusBar, ESlint, Prettier and SearchBar from react-native-paper

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, SafeAreaView, StatusBar } from "react-native";
+import { Searchbar } from "react-native-paper";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.search}>
+          {/* <Text>Search</Text>
+           */}
+          <Searchbar placeholder="Search" />
+        </View>
+        <View style={styles.list}>
+          <Text>List</Text>
+        </View>
+      </SafeAreaView>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: StatusBar.currentHeight,
+  },
+  search: {
+    padding: 15,
+  },
+  list: {
+    flex: 1,
+    backgroundColor: "grey",
+    padding: 15,
   },
 });
