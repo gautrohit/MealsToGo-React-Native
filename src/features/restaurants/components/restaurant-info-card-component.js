059c2f1470955c5a5d83c8d54d5290457bb6d7d9
Rohit Gautam <gautamrohit55@gmail.com>
2022-12-17 16:06:37 +0530

    use two SVG rating, open

--- src/features/restaurants/components/restaurant-info-card-component.js
+++ src/features/restaurants/components/restaurant-info-card-component.js
@@ -1,7 +1,10 @@
 import React from "react";
-import { Text } from "react-native";
+import { Text, View } from "react-native";
 import { Card } from "react-native-paper";
 import styled from "styled-components";
+import { SvgXml } from "react-native-svg";
+import star from "../../../../assets/start"
+import open from "../../../../assets/open"
 
 const RestaurantsCard = styled(Card)`
   backgroundcolor: ${(props) => props.theme.colors.bg.primary};
@@ -11,9 +14,25 @@ const CardCover = styled(Card.Cover)`
   backgroundcolor: ${(props) => props.theme.colors.bg.primary};
 `;
 const Title = styled(Text)`
-  font-family: ${(props) => props.theme.fonts.body}
-  padding: ${(props) => props.theme.space[3]}
+  font-family: ${(props) => props.theme.fonts.heading}
+  font-size: ${(props) => props.theme.fontSizes.body}
   color: ${(props) => props.theme.colors.ui.primary};
+  `;
+const Address = styled(Text)`
+font-family: ${props => props.theme.fonts.body}
+font-size: ${props => props.theme.fontSizes.caption}
+  `;
+const Info = styled(View)`
+padding: ${props => props.theme.space[3]}
+`;
+const Rating = styled(View)`
+flex-direction: row
+padding-top: ${props => props.theme.space[2]}
+padding-bottom: ${props => props.theme.space[2]}
+`
+const Wrap1 = styled(View)`
+flex-direction: row
+justify-content: space-between;
 `;
 
 export const ResturantInfoCard = ({ resturant = {} }) => {
@@ -25,15 +44,31 @@ export const ResturantInfoCard = ({ resturant = {} }) => {
     ],
     address = "VIP Road Raipur",
     isOpenNow = true,
-    rating = 4,
-    isClosedTemporarily,
+    rating = 3.2,
+    isClosedTemporarily = "Close",
   } = resturant;
+
+  const ratingArray = Array.from(new Array(Math.floor(rating)));
 
   return (
     <>
       <RestaurantsCard elevation={5}>
         <CardCover key={name} source={{ uri: photos[0] }} />
-        <Title>{name}</Title>
+        <Info>
+          <Title>{name}</Title>
+          <Wrap1>
+            <Rating>
+              {
+                ratingArray.map(() => {
+                  return <SvgXml xml={star} width={25} height={25} />
+                })
+              }
+            </Rating>
+            {isOpenNow ? <SvgXml xml={open} width={25} height={25} /> : <Text>{isClosedTemporarily}</Text>}
+
+          </Wrap1>
+          <Address>{address}</Address>
+        </Info>
       </RestaurantsCard>
     </>
   );
